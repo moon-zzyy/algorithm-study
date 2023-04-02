2426b8343e33fe6d53204229315ee3d8898654fa
moon-zzyy <gracemjy@konkuk.ac.kr>
2023-04-02 23:58:33 +0900

    dijkstra

--- barkingdog_algo/29_dijkstra.py
+++ barkingdog_algo/29_dijkstra.py
@@ -31,5 +31,44 @@ def boj1753():
             print('INF' if d[i]==INF else d[i])
 
 
+def boj11779():
+    INF = sys.maxsize
+    V = int(input())
+    E = int(input())
+    d = [INF]*(V+1)  # 최단 거리
+    pre = [0]*(V+1) # 최단 거리 경로 복원
+    graph = [[] for _ in range(V+1)] # (w,v)
+    for _ in range(E):
+        u, v, w = map(int, input().split())
+        graph[u].append((w,v))
+    s, e = map(int,input().split()) # 출발, 도착
+
+    queue = []
+    d[s]=0
+    heappush(queue, (d[s], s))
+    while queue:
+        w, v = heappop(queue)
+        # 현재 정점에 대한 가중치가 다르면 넘어감
+        if d[v]!=w: continue
+        for nxt_w, nxt_v in graph[v]:
+            # 현재 정점의 간선 중
+            # 현재 정점을 거치는 것이 더 작으면 갱신
+            if d[nxt_v] > d[v]+nxt_w:
+                d[nxt_v] = d[v]+nxt_w
+                heappush(queue, (d[nxt_v], nxt_v))
+                pre[nxt_v] = v # 이전 정점 저장
+
+    print(d[e])
+    path = []
+    idx = e
+    while idx!=s:
+        path.append(idx)
+        idx = pre[idx]
+    path.append(idx)
+    path.reverse()
+    print(len(path))
+    print(*path, sep=' ')
+
+
 if __name__=='__main__':
-    boj1753()
+    boj11779()
